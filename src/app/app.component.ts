26075d5a7642c4ce372c12d958c5501b4f20785b
hadimaster <hadimaster65555@gmail.com>
2018-01-05 10:54:53 +0700

    Pushing some files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  Persons: Array<any>;
+  constructor() { }
+
+  ngOnInit() {
+    this.Persons = [
+      { nama: 1, email: "Coffees, teas", company: "tabeldata"},
+      { nama: 2, email: "lililili", company:"lalalala"},
+      { nama: 3, email: "lolololo", company: "lelele"}
+    ];
+  }
 }
